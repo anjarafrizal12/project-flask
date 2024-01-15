c26851976c7ce18b5c3ad658e20341d7d22c7b52
Anjar <anjarafrizal12@gmail.com>
2024-01-16 05:07:37 +0700

    update

--- api/index.py
+++ api/index.py
@@ -6,6 +6,20 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/about', methods=["POST"])
-def about():
-    return 'About'
+@app.route('/upload-ui', methods=["POST"])
+def uploadui():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = file.filename
+        name, extension = os.path.splitext(filename)
+
+        print(extension.upper())
+
+        if extension.upper() == ".PNG" or extension.upper() == ".JPG" or extension.upper() == ".JPEG":
+            my_string = base64.b64encode(file.read())
+            my_string = my_string.decode('utf-8')
+
+            return {"Uploaded file": file.filename, "base64":my_string}
+            
+        else:
+            return {"error":"invalid extension","allowed":"JPG, JPEG, PNG","upload_ext":extension.upper()}
