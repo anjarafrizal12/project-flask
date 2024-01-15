62a3173563e9dac26ef503df0af5444fd4863331
Anjar <anjarafrizal12@gmail.com>
2024-01-16 05:01:10 +0700

    update

--- api/index.py
+++ api/index.py
@@ -1,32 +1,11 @@
 from flask import Flask
-from io import BytesIO
-from flask import Flask, render_template, request, send_file
-from flask_sqlalchemy import SQLAlchemy
-import base64
-import os
 
 app = Flask(__name__)
 
-my_string = ""
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route('/upload-ui', methods=["POST"])
-def uploadui():
-    if request.method == 'POST':
-        file = request.files['file']
-        filename = file.filename
-        name, extension = os.path.splitext(filename)
-
-        print(extension.upper())
-
-        if extension.upper() == ".PNG" or extension.upper() == ".JPG" or extension.upper() == ".JPEG":
-            my_string = base64.b64encode(file.read())
-            my_string = my_string.decode('utf-8')
-
-            return {"Uploaded file": file.filename, "base64":my_string}
-            
-        else:
-            return {"error":"invalid extension","allowed":"JPG, JPEG, PNG","upload_ext":extension.upper()}
-
-@app.route('/', methods=["GET"])
-def homepage():
-    return "Hello World"
+@app.route('/about')
+def about():
+    return 'About'
